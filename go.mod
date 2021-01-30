2a5aef56499f8198b9e243d8ad337162312c0ad7
hmoralesaragithub1 <hmoralesara@gmail.com>
2021-01-30 11:46:02 -0500

    quinto commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hmoralesaragithub1/anferdev
 go 1.14
 
 require (
+	github.com/culturadevops/jgt/jio v0.0.0-20201124225139-c0de2ada0cbe
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
--- services/model.go
+++ services/model.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"github.com/culturadevops/jgt/jio"
+)
+
+func createFolderIsNotExist(folderName string) {
+	if !jio.IsFileExist(folderName) {
+		jio.CreateDirAll(folderName)
+	}
+}
+
+func Model(filename string) {
+	createFolderIsNotExist("models")
+	MapForReplace := make(map[string]string)
+	MapForReplace["%MODELNAME%"] = filename
+	newName := "models/" + filename + ".go"
+	templateName := "C:/Users/Soporte/.config/anferdev/model.stub"
+	jio.NewFileforTemplate(newName, templateName, MapForReplace)
+}
